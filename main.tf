33311eba3bfa00cf4753647c6ae859a30e9a2666
meeeeeeeee <me@you.org>
2025-05-20 18:40:54 +0000

    updatexxddd

--- main.tf
+++ main.tf
@@ -13,3 +13,16 @@ resource "aws_sqs_queue" "terraform_queue2" {
   name = var.q2_name
   #fifo_queue = true
 }
+
+resource "aws_cloudwatch_log_group" "this" {
+  count = var.create ? 1 : 0
+
+  name              = var.name
+  name_prefix       = var.name_prefix
+  retention_in_days = var.retention_in_days
+  kms_key_id        = var.kms_key_id
+  log_group_class   = var.log_group_class
+  skip_destroy      = var.skip_destroy
+
+  tags = var.tags
+}
--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,16 @@ output "q_arn" {
   description = "ARN of the q"
   value       = aws_sqs_queue.terraform_queue.arn
 }
+
+/*
+Outputs from CloudWatch LOg Group
+*/
+output "cloudwatch_log_group_name" {
+  description = "Name of Cloudwatch log group"
+  value       = try(aws_cloudwatch_log_group.this[0].name, "")
+}
+
+output "cloudwatch_log_group_arn" {
+  description = "ARN of Cloudwatch log group"
+  value       = try(aws_cloudwatch_log_group.this[0].arn, "")
+}
